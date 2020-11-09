8eac3478a2e799ec248c3995c170f43fb1132f40
Steve Van Hooser <vanhoosr@brandeis.edu>
2020-11-09 15:45:23 -0500

    more grading

--- +vlt/+grade/grade_140_exam_questions.m
+++ +vlt/+grade/grade_140_exam_questions.m
@@ -9,14 +9,19 @@ questions_out = questions_in;
 
 for i=1:numel(questions_out),
 	if isempty(questions_out(i).iscorrect),
+
+		disp(['Question ' int2str(i) ' of ' int2str(numel(questions_out)) '.']);
+
 		disp(newline);
 		disp(newline);
 
 		disp([questions_out(i).aiken])
 
-		r = input('Is the question correct? 1=yes, 0=no, 2=needs attention, -1=abort :');
+		r = input('Is the question correct? 1=yes, 0=no, 2=needs attention, 3=ignoring, 4=not my section, -1=abort :');
 		if r==-1, return; end; % abort
 		questions_out(i).iscorrect = r;
+
+		if r==4, continue; end;
 
 		r = input('Is the question a candidate for the exam? 1=yes, 0=no, 2=maybe , -1=abort :');
 		if r==-1, return; end; % abort
